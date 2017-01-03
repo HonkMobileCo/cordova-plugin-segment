d7a64cfaea570fca0a9d13695d07c3399bd500eb
Ilia Golod <ilia.golod@gmail.com>
2017-01-03 13:23:58 -0500

    get the key from the config file doesn't support the debug mode

--- scripts/replaceMainActivity.js
+++ scripts/replaceMainActivity.js
@@ -5,25 +5,28 @@ module.exports = function(ctx) {
     }
     var fs = ctx.requireCordovaModule('fs'),
         path = ctx.requireCordovaModule('path'),
-        deferral = ctx.requireCordovaModule('q').defer();
+        deferral = ctx.requireCordovaModule('q').defer(),
+        et = ctx.requireCordovaModule('elementtree');
 
     var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
     var mainActivityFileTemplate = path.join(ctx.opts.projectRoot, 'plugins/cordova-plugin-segment/src/android/MainActivity.java');
     var mainActivityFile = path.join(platformRoot, 'src/com/honkmobile/MainActivity.java');
 
+    var configData = fs.readFileSync(path.join(ctx.opts.projectRoot, 'config.xml')).toString();
+    var configDataParsed = et.parse(configData);
+    var prodKey = configDataParsed.findall(".//platform[@name='android']/preference[@name='analytics_write_key']")[0].get('value')
+
     fs.readFile(mainActivityFileTemplate, 'utf8', function (err,data) {
       if (err) {
         return console.log(err);
       }
-      var result = data.replace(/\|SEGMENT_KEY\|/g, 'Key');
-      console.log(result)
-
+      var result = data.replace(/\|SEGMENT_KEY\|/g, prodKey);
 
       fs.writeFile(mainActivityFile, result, 'utf8', function (err) {
          if (err) return console.log(err);
       });
       deferral.resolve();
     });
-
+    console.log("MainActivity File has been updated")
     return deferral.promise;
 };
